c36d6f9822948c59a60b9f552faa2ec1173b5c9f
Rujin Shrestha <63194470+TheNyachhon@users.noreply.github.com>
2022-01-13 12:07:53 +0530

    notes retrieved/saved to local storage 🎒

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const newNote = document.querySelector('#new-note')
 newNote.addEventListener('click', () => {
     addNewNote()
 })
-function addNewNote() {
+// Retrieving data from local storage
+function retrieveNotes() {
+    const retrievedNotes = localStorage.getItem('notesArray')
+    const allNotes = JSON.parse(retrievedNotes)
+    if (allNotes) {
+        allNotes.forEach(note => {
+            addNewNote(note.title, note.note)
+        })
+    }
+}
+retrieveNotes()
+// Adding a new note
+function addNewNote(title = '', notes = '') {
     const main = document.querySelector('main')
     const noteCard = document.createElement('div');
     noteCard.classList.add('note-card')
@@ -12,22 +24,26 @@ function addNewNote() {
         <i class="fas fa-times" onclick="deleteNote(this)"></i>
     </div>
     <div class="title-area">
-        <div class="title"></div>
+        <div class="title">${title}</div>
         <input type="text" class="hidden" placeholder="Enter Title">
     </div>
     <div class="note-area">
-        <div class="note"></div>
+        <div class="note">${notes}</div>
         <textarea class="hidden" placeholder="Enter note here"></textarea>
     </div>`
     main.appendChild(noteCard)
+    saveNotes()
 }
+// Deleting note
 function deleteNote(e) {
     const confirmDel = confirm("Delete note?")
     if (confirmDel) {
         const noteCard = e.parentElement.parentElement;
         noteCard.remove()
     }
+    saveNotes()
 }
+// Editing note
 function edit(e) {
     const noteCard = e.parentElement.parentElement;
     const textarea = noteCard.querySelector('textarea');
@@ -36,16 +52,32 @@ function edit(e) {
     const input = noteCard.querySelector('input');
     title.classList.toggle('hidden')
     note.classList.toggle('hidden')
-    if(e.classList.contains('save-note')){
+    if (e.classList.contains('save-note')) {
         note.innerText = textarea.value
         title.innerText = input.value
-    }else{
+    } else {
         textarea.value = note.innerText
         input.value = title.innerText
     }
+    saveNotes()
     e.classList.toggle('fa-edit')
     e.classList.toggle('fa-save')
     e.classList.toggle('save-note')
     textarea.classList.toggle('hidden')
     input.classList.toggle('hidden')
+}
+// Local storage
+function saveNotes() {
+    const allNotes = document.querySelectorAll('.note-card')
+    
+    const notesArray = []
+    allNotes.forEach(note => {
+        const title = note.querySelector('.title').innerText
+        const noteText = note.querySelector('.note').innerText
+        notesArray.push({
+            title: title,
+            note: noteText
+        })
+    })
+    localStorage.setItem('notesArray', JSON.stringify(notesArray))
 }
